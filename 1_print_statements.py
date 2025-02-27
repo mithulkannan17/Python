0dd3356aba0e1e9198cb2457f150e3d356bcbf75
MITHUL KANNAN <2023ci_mithulkannan_a@nie.ac.in>
2025-02-27 22:36:30 +0530

    add

---
+++ 1_print_statements.py
@@ -0,0 +1,15 @@
+# To use print function, we can simply call it with the message we want to display.
+
+print("Hello World!")
+
+# for printfing in a new line use \n.
+print("\n")
+print("This is a Python program\nI am learning Python\nThis is the end of the program")
+
+# for printing with spaces, use \t.
+print("\n")
+print("\tThis is the first line")
+print("\tThis is the second line")
+
+# The print function displays the given message on the console or terminal. In this case, it displayed "Hello World!" on the console.
+
---
+++ 2_veriable_print.py
@@ -0,0 +1,16 @@
+# veriable declaration and assignment
+s = "Hello World!"
+
+# print statement
+
+print(s)
+
+# muliple veriable declaration and assignment
+ 
+s1 = "This is a Python program."
+
+s2 = "I am learning Python"
+
+s3 = "This is the end of the program"
+
+print(s1, s2, s3)
